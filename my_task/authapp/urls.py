160490e029b128dd351527f807d1e072ecb7c66b
Rohit <rohitkumarbxr243@gmail.com>
2024-05-01 14:51:33 +0530

    class based function

--- my_task/authapp/urls.py
+++ my_task/authapp/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
 from rest_framework import routers
-from .views import register_view, login_view, admin_register_view, register_product, product_search, user_search, order_initiate, transaction_view
+from .views import UserViewset, LoginViewset, AdminRegister, RegisterProduct, ProductSearch, UserSearch, OrderInitiate, TransactionView
 
 
 urlpatterns = [
-    path('register_view/', register_view),
-    path('login_view/', login_view),
-    path('admin_register/', admin_register_view),
-    path('register_product/', register_product),
-    path('product_search/', product_search),
-    path('user_search/', user_search),
-    path('order_create/', order_initiate),
-    path('order_complete/', transaction_view),
+    path('register_view/', UserViewset.as_view()),
+    path('login_view/', LoginViewset.as_view()),
+    path('admin_register/', AdminRegister.as_view()),
+    path('register_product/', RegisterProduct.as_view()),
+    path('product_search/', ProductSearch.as_view()),
+    path('user_search/', UserSearch.as_view()),
+    path('order_create/', OrderInitiate.as_view()),
+    path('order_complete/', TransactionView.as_view()),
 
     # Add other URLs as needed
     ]
--- my_task/authapp/views.py
+++ my_task/authapp/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.sessions.models import Session
 from django.contrib.auth.hashers import check_password
@@ -18,9 +19,21 @@ from .serializers import UserSerializer, UserLoginSerializer, ProductSerializer,
 
 
 
-@api_view(['POST', 'GET', 'PATCH', 'DELETE'])
-def register_view(request):
-    if request.method=='POST':
+
+
+
+# ==============================================================================================
+
+class UserViewset(APIView):
+     
+    def get(self, request):
+        query_data=UserDetails.objects.all()
+        if query_data.exists():
+            task_data=UserSerializer(query_data, many=True)
+            return Response(task_data.data, status=status.HTTP_200_OK)
+        return Response({"message": "No User found"}, status=status.HTTP_404_NOT_FOUND)
+     
+    def post(self, request):
         data=request.data
         email1=data.get('email')
         password1=data.get('password')
@@ -47,25 +60,9 @@ def register_view(request):
             "message":"Data is not valid"
         }, status=status.HTTP_400_BAD_REQUEST)
     
-    elif request.method=='GET':
-        query_data=UserDetails.objects.all()
-        if query_data.exists():
-            task_data=UserSerializer(query_data, many=True)
-            return Response(task_data.data, status=status.HTTP_200_OK)
-        return Response({"message": "No User found"}, status=status.HTTP_404_NOT_FOUND)
-    
 
-    elif request.method == 'DELETE':
-            query_data=UserDetails.objects.filter(id=request.data.get('id'))
-            query_data_login=UserLogin.objects.filter(id=request.data.get('id'))
-            if query_data.exists():
-                query_data.delete()
-                query_data_login.delete()
-                return Response({'msg': 'User deleted successfully'}, status=status.HTTP_200_OK)
-            return Response({"message": "No user found"}, status=status.HTTP_404_NOT_FOUND)
-        
 
-    elif request.method == 'PATCH':
+    def patch(self, request):
             query_data=UserDetails.objects.get(id=request.data.get('id'))
             serializer = UserSerializer(query_data, data=request.data, partial=True)
             if serializer.is_valid():
@@ -77,208 +74,218 @@ def register_view(request):
                 return Response({'msg': 'User updated successfully'}, status=status.HTTP_200_OK)
             return Response({"message": "No User found"}, status=status.HTTP_404_NOT_FOUND)
     
-    else:
-            response={
-            "meggage": "It can not processed",
-            }
-            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        query_data=UserDetails.objects.filter(id=request.data.get('id'))
+        query_data_login=UserLogin.objects.filter(id=request.data.get('id'))
+        if query_data.exists():
+            query_data.delete()
+            query_data_login.delete()
+            return Response({'msg': 'User deleted successfully'}, status=status.HTTP_200_OK)
+        return Response({"message": "No user found"}, status=status.HTTP_404_NOT_FOUND)
+
     
-    
+# =========================================================================================================
+# =========================================================================================================
+# =========================================================================================================
 
+class AdminRegister(APIView):
+    def post(self, request):
+        data = request.data
+        email1 = data.get('email')
 
-@api_view(['POST'])
-def admin_register_view(request):
-    data = request.data
-    email1 = data.get('email')
-
-    if AdminLogin.objects.filter(email=email1).exists():
-        return Response({
-            "message":"email is already registered"
-        }, status=status.HTTP_400_BAD_REQUEST)
-    else:
-        serializer = AdminLoginSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response({
-            "message":"Data is not valid"
-        }, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-
-@api_view(['POST'])
-def login_view(request):
-    # how to store response in local storage in react
-    data = request.data
-    email = data.get("email")
-    password = data.get("password")
-    admin_login_data = AdminLogin.objects.filter(email=email, password=password)
-    login_data = UserLogin.objects.filter(email=email, password=password)
-    # user_data = UserLoginSerializer(user)
-    # print(user_data.data)
-
-    if admin_login_data:
-        user = authenticate(request, username=email, password=password)
-        if user is not None:
-            login(request, user)
-            # set user-specific data in the session
-            request.session['username'] = email
-            request.session['is_logged_in'] = True
-            request.session.save()
-            return Response({"message": "login success"}, status=status.HTTP_200_OK)
+        if AdminLogin.objects.filter(email=email1).exists():
+            return Response({
+                "message":"email is already registered"
+            }, status=status.HTTP_400_BAD_REQUEST)
         else:
-            response={
-        "message": "Authentication failed for admin",
-        }
-        return Response(response, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-    elif login_data:
-        token = generate_jwt(data)
-        queryset=UserDetails.objects.get(email=data.get("email"))
-        user_data=UserSerializer(queryset)
-        response = {
-        "meggage": "Logined success",
-        "jwt": token,
-        "u_data": user_data.data
-        }
-        # response.set_cookie(key="jwt", value=token)
-        return Response(response, status=status.HTTP_200_OK)
-    else:
-        response={
-        "meggage": "Invalid Username or Password",
-        }
-        return Response(response, status=status.HTTP_400_BAD_REQUEST)
-    
-
-
-
-
-@api_view(['POST','GET','PATCH','DELETE'])
-def register_product(request):
-        if request.method == 'POST':
-            data = request.data
-            serializer = ProductSerializer(data=data)
+            serializer = AdminLoginSerializer(data=data)
             if serializer.is_valid():
                 serializer.save()
-                return Response({"message" : "product inserted succesfully"}, status=status.HTTP_200_OK)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response({
+                "message":"Data is not valid"
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+class LoginViewset(APIView):
+
+    def post(self, request):
+        # how to store response in local storage in react
+        data = request.data
+        email = data.get("email")
+        password = data.get("password")
+        admin_login_data = AdminLogin.objects.filter(email=email, password=password)
+        login_data = UserLogin.objects.filter(email=email, password=password)
+        # user_data = UserLoginSerializer(user)
+        # print(user_data.data)
+
+        if admin_login_data:
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                # set user-specific data in the session
+                request.session['username'] = email
+                request.session['is_logged_in'] = True
+                request.session.save()
+                return Response({"message": "login success"}, status=status.HTTP_200_OK)
             else:
                 response={
-            "meggage": "Invalid data",
+            "message": "Authentication failed for admin",
             }
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
-    
-        elif request.method == 'GET':
-            query_data=Products.objects.all()
-            if query_data.exists():
-                paginator = PageNumberPagination()
-                paginator.page_size = 2
-                paginationdata = paginator.paginate_queryset(query_data, request)
-                serializer=ProductSerializer(paginationdata, many=True)
-                return paginator.get_paginated_response(serializer.data)
-            return Response({"message": "No User found"}, status=status.HTTP_404_NOT_FOUND)
-        
 
-        elif request.method == 'DELETE':
-            query_data=Products.objects.filter(id=request.data.get('id'))
-            if query_data.exists():
-                query_data.delete()
-                return Response({'msg': 'Product deleted successfully'}, status=status.HTTP_200_OK)
-            return Response({"message": "No product found"}, status=status.HTTP_404_NOT_FOUND)
-        
 
-        elif request.method == 'PATCH':
-            query_data=Products.objects.get(id=request.data.get('id'))
-            serializer = ProductSerializer(query_data, data=request.data, partial=True)
-            if serializer.is_valid():
-                serializer.save()
-                return Response({'msg': 'Product updated successfully'}, status=status.HTTP_200_OK)
-            return Response({"message": "No product found"}, status=status.HTTP_404_NOT_FOUND)
-    
+
+        elif login_data:
+            token = generate_jwt(data)
+            queryset=UserDetails.objects.get(email=data.get("email"))
+            user_data=UserSerializer(queryset)
+            response = {
+            "meggage": "Logined success",
+            "jwt": token,
+            "u_data": user_data.data
+            }
+            # response.set_cookie(key="jwt", value=token)
+            return Response(response, status=status.HTTP_200_OK)
         else:
             response={
-            "meggage": "It can not processed",
+            "meggage": "Invalid Username or Password",
             }
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
         
-    
-@api_view(['POST'])
-def product_search(request):
-    if request.data is None:
-         return Response({"message": "Please Enter data which you want to find"}, status=status.HTTP_404_NOT_FOUND)
-    if request.method == "POST":
-        data = request.data.get('name')
-        query_data = Products.objects.filter(product_name__icontains=data)
+
+# ==========================================================================================================
+# ==========================================================================================================
+# ==========================================================================================================
+
+
+class RegisterProduct(APIView):
+    def post(self, request):
+        data = request.data
+        serializer = ProductSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message" : "product inserted succesfully"}, status=status.HTTP_200_OK)
+        else:
+            response={
+        "meggage": "Invalid data",
+        }
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def get(self, request):
+        query_data=Products.objects.all()
         if query_data.exists():
-            task_data = ProductSerializer(query_data, many=True)
-            return Response(task_data.data, status=status.HTTP_200_OK)
-        return Response({"message": "No data found"}, status=status.HTTP_404_NOT_FOUND)
+            paginator = PageNumberPagination()
+            paginator.page_size = 2
+            paginationdata = paginator.paginate_queryset(query_data, request)
+            serializer=ProductSerializer(paginationdata, many=True)
+            return paginator.get_paginated_response(serializer.data)
+        return Response({"message": "No User found"}, status=status.HTTP_404_NOT_FOUND)
     
 
-    
-@api_view(['POST'])
-def user_search(request):
-    if request.method == "POST":
-        data = request.data.get('name')
-        query_data = UserDetails.objects.filter(name__icontains=data)
+    def delete(self, request):
+        query_data=Products.objects.filter(id=request.data.get('id'))
         if query_data.exists():
-            task_data = UserSerializer(query_data, many=True)
-            return Response(task_data.data, status=status.HTTP_200_OK)
-        return Response({"message": "No data found"}, status=status.HTTP_404_NOT_FOUND)
+            query_data.delete()
+            return Response({'msg': 'Product deleted successfully'}, status=status.HTTP_200_OK)
+        return Response({"message": "No product found"}, status=status.HTTP_404_NOT_FOUND)
     
 
+    def patch(self, request):
+        query_data=Products.objects.get(id=request.data.get('id'))
+        serializer = ProductSerializer(query_data, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Product updated successfully'}, status=status.HTTP_200_OK)
+        return Response({"message": "No product found"}, status=status.HTTP_404_NOT_FOUND)
 
-    # =======================================Razorpay==================================================
 
 
-rz_client = RazorpayClient()
-@api_view(['POST'])
-def order_initiate(request):
-    razorpay_order_serializer = RazorpayOrderSerializer(
-            data=request.data
-        )
-    if razorpay_order_serializer.is_valid():
-            order_response = rz_client.create_order(
-                amount=razorpay_order_serializer.validated_data.get("amount"),
-                currency=razorpay_order_serializer.validated_data.get("currency")
+# ==================================================================================================================
+# ============================================Searching=============================================================
+# ==================================================================================================================
+    
+class ProductSearch(APIView):
+    def post(self, request):
+        if request.data is None:
+            return Response({"message": "Please Enter data which you want to find"}, status=status.HTTP_404_NOT_FOUND)
+        if request.method == "POST":
+            data = request.data.get('name')
+            query_data = Products.objects.filter(product_name__icontains=data)
+            if query_data.exists():
+                task_data = ProductSerializer(query_data, many=True)
+                return Response(task_data.data, status=status.HTTP_200_OK)
+            return Response({"message": "No data found"}, status=status.HTTP_404_NOT_FOUND)
+    
+
+    
+class UserSearch(APIView):
+    def post(self, request):
+        if request.method == "POST":
+            data = request.data.get('name')
+            query_data = UserDetails.objects.filter(name__icontains=data)
+            if query_data.exists():
+                task_data = UserSerializer(query_data, many=True)
+                return Response(task_data.data, status=status.HTTP_200_OK)
+            return Response({"message": "No data found"}, status=status.HTTP_404_NOT_FOUND)
+    
+
+#===================================================Razorpay=======================================================
+#===================================================Razorpay=======================================================
+#===================================================Razorpay=======================================================
+
+
+class OrderInitiate(APIView):
+    
+    def post(self, request):
+        razorpay_order_serializer = RazorpayOrderSerializer(
+                data=request.data
             )
-            response = {
-                "status_code": status.HTTP_201_CREATED,
-                "message": "order created",
-                "data": order_response,
-                
-            }
-            return Response(response, status=status.HTTP_201_CREATED)
-    else:
-            response = {
-                "status_code": status.HTTP_400_BAD_REQUEST,
-                "message": "bad request",
-                "error": razorpay_order_serializer.errors
-            }
-            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        if razorpay_order_serializer.is_valid():
+                rz_client = RazorpayClient()
+                order_response = rz_client.create_order(
+                    amount=razorpay_order_serializer.validated_data.get("amount"),
+                    currency=razorpay_order_serializer.validated_data.get("currency")
+                )
+                response = {
+                    "status_code": status.HTTP_201_CREATED,
+                    "message": "order created",
+                    "data": order_response,
+                    
+                }
+                return Response(response, status=status.HTTP_201_CREATED)
+        else:
+                response = {
+                    "status_code": status.HTTP_400_BAD_REQUEST,
+                    "message": "bad request",
+                    "error": razorpay_order_serializer.errors
+                }
+                return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['POST'])
-def transaction_view(request):
-    transaction_serializer = TranscationModelSerializer(data=request.data)
-    if transaction_serializer.is_valid():
-            rz_client.verify_payment_signature(
-                razorpay_payment_id = transaction_serializer.validated_data.get("payment_id"),
-                razorpay_order_id = transaction_serializer.validated_data.get("order_id"),
-                razorpay_signature = transaction_serializer.validated_data.get("signature")
-            )
-            transaction_serializer.save()
-            response = {
-                "status_code": status.HTTP_201_CREATED,
-                "message": "transaction created"
-            }
-            return Response(response, status=status.HTTP_201_CREATED)
-    else:
-            response = {
-                "status_code": status.HTTP_400_BAD_REQUEST,
-                "message": "bad request",
-                "error": transaction_serializer.errors
-            }
-            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+class TransactionView(APIView):
+    def post(self, request):
+        transaction_serializer = TranscationModelSerializer(data=request.data)
+        if transaction_serializer.is_valid():
+                rz_client = RazorpayClient()
+                rz_client.verify_payment_signature(
+                    razorpay_payment_id = transaction_serializer.validated_data.get("payment_id"),
+                    razorpay_order_id = transaction_serializer.validated_data.get("order_id"),
+                    razorpay_signature = transaction_serializer.validated_data.get("signature")
+                )
+                transaction_serializer.save()
+                response = {
+                    "status_code": status.HTTP_201_CREATED,
+                    "message": "transaction created"
+                }
+                return Response(response, status=status.HTTP_201_CREATED)
+        else:
+                response = {
+                    "status_code": status.HTTP_400_BAD_REQUEST,
+                    "message": "bad request",
+                    "error": transaction_serializer.errors
+                }
+                return Response(response, status=status.HTTP_400_BAD_REQUEST)
