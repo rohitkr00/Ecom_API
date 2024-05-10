20277110f74c2e483219e5d5f8060cb67ee26e05
Rohit <rohitkumarbxr243@gmail.com>
2024-05-10 12:19:29 +0530

    db

--- my_task/my_task/settings.py
+++ my_task/my_task/settings.py
@@ -75,14 +75,22 @@ WSGI_APPLICATION = 'my_task.wsgi.application'
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.fallback.FallbackStorage'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'hktask_a',
+#         'USER': 'root',
+#         'PASSWORD': 'root',
+#         'HOST':'localhost',
+#         'PORT':'3306',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'hktask_a',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
